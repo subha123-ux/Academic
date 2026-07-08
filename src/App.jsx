acbc9efbe4b8890bf975c88439d5171f84793bb7
subha123-ux <subhajitmal509@gmail.com>
2026-07-08 13:14:36 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,5 @@
-// import { Routes, Route } from "react-router-dom";
-
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-
-// import Home from "./pages/Home";
-// import Courses from "./pages/Courses";
-// import Gallery from "./pages/Gallery";
-// import Contact from "./pages/Contact";
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/courses" element={<Courses />} />
-//         <Route path="/gallery" element={<Gallery />} />
-//         <Route path="/contact" element={<Contact />} />
-//       </Routes>
-
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
 
 import { Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import BackToTop from "./components/BackToTop";
